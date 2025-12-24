4822be3d2dd3635c6749445b20dd5b618f9f78b6
FayBlaze <ichimiyaaltra@gmail.com>
2025-12-24 20:00:17 +0700

    final

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -1,34 +1,42 @@
-from pathlib import Path
+import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, classification_report
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "titanic_preprocessing")  # folder ada di MLproject/
 
-# Load data
-X_train = pd.read_csv("membangun_model/titanic_preprocessing/X_train.csv")
-X_test = pd.read_csv("membangun_model/titanic_preprocessing/X_test.csv")
-y_train = pd.read_csv("membangun_model/titanic_preprocessing/y_train.csv").values.ravel()
-y_test = pd.read_csv("membangun_model/titanic_preprocessing/y_test.csv").values.ravel()
+def load_data():
+    X_train = pd.read_csv(os.path.join(DATA_DIR, "X_train.csv"))
+    X_valid = pd.read_csv(os.path.join(DATA_DIR, "X_valid.csv"))
+    y_train = pd.read_csv(os.path.join(DATA_DIR, "y_train.csv")).values.ravel()
+    y_valid = pd.read_csv(os.path.join(DATA_DIR, "y_valid.csv")).values.ravel()
+    return X_train, X_valid, y_train, y_valid
 
-# MLflow setup
-mlflow.set_experiment("Titanic-MLflow-Basic")
-mlflow.sklearn.autolog(log_models=True)
+def main():
+    mlflow.set_tracking_uri("file://" + os.path.join(BASE_DIR, "mlruns"))
+    mlflow.set_experiment("Titanic-MLflow-Basic")
+    mlflow.sklearn.autolog(log_models=True)
 
-# Start run
-with mlflow.start_run(run_name="logreg_baseline"):
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+    X_train, X_valid, y_train, y_valid = load_data()
 
-    preds = model.predict(X_test)
-    proba = model.predict_proba(X_test)[:, 1]
+    with mlflow.start_run(run_name="logreg_baseline"):
+        model = LogisticRegression(max_iter=1000)
+        model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, preds)
-    f1 = f1_score(y_test, preds)
-    auc = roc_auc_score(y_test, proba)
+        preds = model.predict(X_valid)
+        proba = model.predict_proba(X_valid)[:, 1]
 
-    mlflow.log_metric("val_accuracy", acc)
-    mlflow.log_metric("val_f1", f1)
-    mlflow.log_metric("val_auc", auc)
+        acc = accuracy_score(y_valid, preds)
+        f1 = f1_score(y_valid, preds)
+        auc = roc_auc_score(y_valid, proba)
+
+        mlflow.log_metric("val_accuracy", acc)
+        mlflow.log_metric("val_f1", f1)
+        mlflow.log_metric("val_auc", auc)
+
+if __name__ == "__main__":
+    main()
